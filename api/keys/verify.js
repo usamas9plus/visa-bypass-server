3bd8b4e23fbf393ed2e4d98c7a85f9db9997f9ad
Usama <usamas9plus@gmail.com>
2026-01-29 16:40:52 +0500

    fix: increase heartbeat timeout to 5m

--- api/keys/verify.js
+++ api/keys/verify.js
@@ -12,7 +12,8 @@ const { createToken } = require('../../lib/crypto');
 const SIGN_SECRET = 'vecna-sign-key';
 
 // Heartbeat must be within this many milliseconds to be considered "online"
-const HEARTBEAT_TIMEOUT = 60000; // 60 seconds
+// Heartbeat must be within this many milliseconds to be considered "online"
+const HEARTBEAT_TIMEOUT = 300000; // 5 minutes (300 seconds)
 
 module.exports = async function handler(req, res) {
     // Handle CORS preflight
